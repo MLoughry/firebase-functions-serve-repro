d7baf3587c853c7ffe480750dbabacca08e24dc5
Michael Loughry <miclo@microsoft.com>
2018-04-01 20:53:05 -0700

    Use the right filename

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,8 @@ import * as functions from 'firebase-functions';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+console.log(functions.config().cert.message);
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send(functions.config().helloworld.message);
+    response.send(functions.config().cert.message);
 });
